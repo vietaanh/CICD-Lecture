c33332934f8116ca15adc4cf11893927a07a9f5b
meyerni88456 <meyerni88456@th-nuernberg.de>
2025-06-27 01:10:53 +0200

    falsify test, rename yml files, correct spelling error

--- src/test/java/th/nuernberg/testverfahren/CalculatorTest.java
+++ src/test/java/th/nuernberg/testverfahren/CalculatorTest.java
@@ -14,7 +14,7 @@ public class CalculatorTest {
     @Test
     void testDivide() {
         Calculator calc = new Calculator();
-        assertEquals(2.0, calc.divide(6, 3));
+        assertEquals(2.0, calc.divide(6, 2));
     }
 
     @Test
