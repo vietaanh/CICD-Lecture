e76275bc6aeab949a4ca0b9d52fdae61afb7d4bd
meyerni88456 <meyerni88456@th-nuernberg.de>
2025-06-26 14:28:17 +0200

    extra lint errors removed, as they are useless

--- src/main/java/th/nuernberg/testverfahren/Calculator.java
+++ src/main/java/th/nuernberg/testverfahren/Calculator.java
@@ -1,9 +1,8 @@
 package th.nuernberg.testverfahren;
 
 public class Calculator {
-    public int add(int a, int b)
-    {
-            return a + b;
+    public int add(int a, int b) {
+        return a + b;
     }
 
     public double divide(int a, int b) {
