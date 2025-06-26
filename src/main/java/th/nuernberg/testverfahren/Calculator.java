3f013cf54770449fd5068addfff8755d59aae68a
meyerni88456 <meyerni88456@th-nuernberg.de>
2025-06-26 14:21:00 +0200

    some qol changes, added linter step and lint problems

--- src/main/java/th/nuernberg/testverfahren/Calculator.java
+++ src/main/java/th/nuernberg/testverfahren/Calculator.java
@@ -1,8 +1,9 @@
 package th.nuernberg.testverfahren;
 
 public class Calculator {
-    public int add(int a, int b) {
-        return a + b;
+    public int add(int a, int b)
+    {
+            return a + b;
     }
 
     public double divide(int a, int b) {
